ad608c36642e404d1bf5f1bd31e943083ccd7c8c
David Ahern <dsahern@gmail.com>
2013-05-25 17:54:00 -0600

    ----perf evsel: Fix printing of perf_event_paranoid message
    commit b69e63a45f2d96a0dfe930ed20385058d4574d2f upstream.
    
    message is currently shown as:
    
      Error:
      You may not have permission to collect %sstats.
      Consider tweaking /proc/sys/kernel/perf_event_paranoid:
    
    Note the %sstats. With patch this becomes:
    
      Error:
      You may not have permission to collect stats.
      Consider tweaking /proc/sys/kernel/perf_event_paranoid:
    
    Signed-off-by: David Ahern <dsahern@gmail.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Link: http://lkml.kernel.org/r/1369526040-1368-1-git-send-email-dsahern@gmail.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: xiakaixu <xiakaixu@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -1514,7 +1514,7 @@ int perf_evsel__open_strerror(struct perf_evsel *evsel,
 	switch (err) {
 	case EPERM:
 	case EACCES:
-		return scnprintf(msg, size,
+		return scnprintf(msg, size, "%s",
 			"You may not have permission to collect %sstats.\n\n"
 			"Consider tweaking /proc/sys/kernel/perf_event_paranoid,\n"
 			"which controls use of the performance events system by\n"
