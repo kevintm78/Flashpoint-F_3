d491ab5ded926ac162aedb352581e1639470ceec
kevintm78 <kevintm78@gmail.com>
2017-09-06 22:51:44 -0400

    fix build

--- drivers/usb/dwc3/ep0.c
+++ drivers/usb/dwc3/ep0.c
@@ -765,6 +765,8 @@ static int dwc3_ep0_std_request(struct dwc3 *dwc, struct usb_ctrlrequest *ctrl)
 		break;
 	case USB_REQ_SET_INTERFACE:
 		dev_vdbg(dwc->dev, "USB_REQ_SET_INTERFACE\n");
+		ret = dwc3_ep0_set_interface(dwc, ctrl);
+		break;
 		dwc->start_config_issued = false;
 		/* Fall through */
 	default:
