84295c28a574fa22e719349c7c98a8c7f74e094b
kevintm78 <kevintm78@gmail.com>
2017-12-12 02:12:42 -0500

    Add Build Script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=$(pwd)/arm-eabi-4.8/bin/arm-eabi-
+mkdir output
+
+make -C $(pwd) O=output apq8084_sec_defconfig VARIANT_DEFCONFIG=apq8084_sec_trlte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j64 -C $(pwd) O=output
+
+cp output/arch/arm/boot/zImage $(pwd)/AIK/split_img/boot.img-zImage
+./tools/dtbTool -o ./AIK/split_img/boot.img-dtb -s 4096 -p ./output/scripts/dtc/ ./output/arch/arm/boot/dts/
+./AIK/repackimg.sh
+mv ./AIK/image-new.img ./boot.img
+rm ./AIK/split_img/boot.img-dtb
+rm ./AIK/split_img/boot.img-zImage
+rm -rf output/
+make clean && make mrproper
