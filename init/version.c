880641118a46aec9d9047e9061404a4f40347314
kevintm78 <kevintm78@gmail.com>
2017-12-12 02:08:21 -0500

    Name the kernel

--- init/version.c
+++ init/version.c
@@ -46,5 +46,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
+	" (Modest.Kernel_by_kevintm78)"
 	" (" LINUX_COMPILER ") %s\n";
